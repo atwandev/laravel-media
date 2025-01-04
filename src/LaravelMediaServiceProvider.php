2246459e5f81e65581e1984d326f968a2de1c8a6
Mohammadreza Atwan <atwandev@gmail.com>
2025-01-04 13:46:42 +0330

    Add service provider and config files

---
+++ src/LaravelMediaServiceProvider.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace AtwanDev\LaravelMedia;
+
+use Illuminate\Support\ServiceProvider;
+
+class LaravelMediaServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
+        $this->mergeConfigFrom(__DIR__ . '/../config/laravel-media.php', 'laravel-media');
+    }
+
+    public function boot()
+    {
+
+    }
+}
