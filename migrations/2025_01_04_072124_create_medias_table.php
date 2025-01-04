5b6c5ee9572a9c57b5b07d0e6094389cd7d1911b
Mohammadreza Atwan <atwandev@gmail.com>
2025-01-04 13:47:55 +0330

    Add `migration` and `MediaTypeEnum`

---
+++ migrations/2025_01_04_072124_create_medias_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use AtwanDev\LaravelMedia\Enums\MediaTypeEnum;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('medias', function (Blueprint $table) {
+            $table->id();
+            $table->enum('type', get_enum_values(MediaTypeEnum::cases()));
+            $table->boolean('is_private');
+            $table->string('filename');
+            $table->text('path');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('medias');
+    }
+};
---
+++ src/Enums/MediaTypeEnum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace AtwanDev\LaravelMedia\Enums;
+
+enum MediaTypeEnum:string
+{
+    case TYPE_IMAGE = 'image';
+    case TYPE_VIDEO = 'video';
+    case TYPE_ZIP = 'zip';
+    case TYPE_AUDIO = 'audio';
+    case TYPE_DOCUMENT = 'document';
+}
