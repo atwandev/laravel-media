4b617d0c4ad68074f5960cb7cbe2cc52dfcd25d6
Mohammadreza Atwan <atwandev@gmail.com>
2025-01-18 18:07:28 +0330

    Create `simple TestCase`

---
+++ tests/Feature/UploadMediaTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace AtwanDev\LaravelMedia\Tests\Feature;
+
+// use IlluminateFoundationTestingRefreshDatabase;
+
+use AtwanDev\LaravelMedia\Tests\TestCase;
+use TestsTestCase;
+
+class UploadMediaTest extends TestCase
+{
+    public function test_is_equals()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
