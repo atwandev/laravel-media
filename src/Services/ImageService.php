b77aae5ef5a3018c6bba3e67477799b13867ae2e
Mohammadreza Atwan <atwandev@gmail.com>
2025-01-18 12:18:48 +0330

    Add `upload function` to `ImageService`

---
+++ src/Services/ImageService.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace AtwanDev\LaravelMedia\Services;
+
+use Illuminate\Http\UploadedFile;
+
+class ImageService
+{
+    public function upload(UploadedFile $file, string $filename, string $dir)
+    {
+        //
+    }
+}
