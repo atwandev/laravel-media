339fb96b7a27ee9fd2c69cff91b0831d6a899fe4
Mohammadreza Atwan <atwandev@gmail.com>
2025-01-18 18:07:05 +0330

    Bind `image-service facade` in `Service Provider`

--- src/LaravelMediaServiceProvider.php
+++ src/LaravelMediaServiceProvider.php
@@ -2,18 +2,21 @@
 
 namespace AtwanDev\LaravelMedia;
 
+use AtwanDev\LaravelMedia\Services\ImageService;
 use Illuminate\Support\ServiceProvider;
 
 class LaravelMediaServiceProvider extends ServiceProvider
 {
     public function register()
     {
-        $this->loadMigrationsFrom(__DIR__ . '/../migrations');
-        $this->mergeConfigFrom(__DIR__ . '/../config/laravel-media.php', 'laravel-media');
+        $this->loadMigrationsFrom(__DIR__.'/../migrations');
+        $this->mergeConfigFrom(__DIR__.'/../config/laravel-media.php', 'laravel-media');
     }
 
     public function boot()
     {
-
+        $this->app->bind('image-service', function () {
+            return new ImageService;
+        });
     }
 }
